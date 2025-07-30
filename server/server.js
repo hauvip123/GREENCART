98d2d28cf3fbf26408d1d3bedfa9215e3061cdaa
hauvip123 <nguyenhaubatri2020@gmail.com>
2025-07-30 09:04:02 +0700

    success product

--- server/server.js
+++ server/server.js
@@ -5,9 +5,12 @@ import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
 const app = express();
 const port = process.env.PORT || 4000;
 await connectDB();
+await connectCloudinary();
 // Allow multiple origins
 const allowerOrigins = ["http://localhost:5173"];
 // Middleware configuration
@@ -18,6 +21,7 @@ app.use(cors({ origin: allowerOrigins, credential: true }));
 app.get("/", (req, res) => res.send("API is Working "));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
